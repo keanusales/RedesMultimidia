ee4b57581a87ab18abc2c502b4cc5ab19a57cb37
GabrielRabello <gabriel.rabello@dgtallab.com.br>
2025-02-01 13:53:18 -0300

    feature: adicionado empacotamento RTP do vídeo no servidor e PLAY no cliente.

--- VideoStreamingCode/RtpPacket.py
+++ VideoStreamingCode/RtpPacket.py
@@ -5,22 +5,25 @@ class RtpPacket:
 	header = bytearray(HEADER_SIZE)
 	
 	def __init__(self):
-		pass
+		self.payload = bytearray()
 		
 	def encode(self, version, padding, extension, cc, seqnum, marker, pt, ssrc, payload):
 		"""Encode the RTP packet with header fields and payload."""
 		timestamp = int(time())
-		header = bytearray(HEADER_SIZE)
-		#--------------
-		# TO COMPLETE
-		#--------------
-		# Fill the header bytearray with RTP header fields
-		
-		# header[0] = ...
-		# ...
-		
-		# Get the payload from the argument
-		# self.payload = ...
+		self.header[0] = (version << 6) | (padding << 5) | (extension << 4) | cc
+		self.header[1] = (marker << 7) | pt
+		self.header[2] = (seqnum >> 8) & 0xFF
+		self.header[3] = seqnum & 0xFF
+		self.header[4] = (timestamp >> 24) & 0xFF
+		self.header[5] = (timestamp >> 16) & 0xFF
+		self.header[6] = (timestamp >> 8) & 0xFF
+		self.header[7] = timestamp & 0xFF
+		self.header[8] = (ssrc >> 24) & 0xFF
+		self.header[9] = (ssrc >> 16) & 0xFF
+		self.header[10] = (ssrc >> 8) & 0xFF
+		self.header[11] = ssrc & 0xFF
+
+		self.payload = payload
 		
 	def decode(self, byteStream):
 		"""Decode the RTP packet."""
